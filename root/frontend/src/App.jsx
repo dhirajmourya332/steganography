f36b6adf95e1763182d30018853640c34d9b4608
dhirajmourya332 <dhirajmourya332@gmail.com>
2023-07-18 09:43:37 +0530

    seprated frontend and backend to reduce onrender runtime

--- root/frontend/src/App.jsx
+++ root/frontend/src/App.jsx
@@ -7,13 +7,13 @@ function App() {
       <Header />
       <StegContainer
         stegTechnique={"Linear LSB"}
-        encodeUri={"/api/steg/llsb/encode"}
-        decodeUri={"/api/steg/llsb/decode"}
+        encodeUri={"https://stego-back.onrender.com/api/steg/llsb/encode"}
+        decodeUri={"https://stego-back.onrender.com/api/steg/llsb/decode"}
       />
       <StegContainer
         stegTechnique={"Random LSB"}
-        encodeUri={"/api/steg/rlsb/encode"}
-        decodeUri={"/api/steg/rlsb/decode"}
+        encodeUri={"https://stego-back.onrender.com/api/steg/rlsb/encode"}
+        decodeUri={"https://stego-back.onrender.com/api/steg/rlsb/decode"}
       />
     </div>
   );
